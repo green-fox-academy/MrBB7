59560c3d47efd61cc3c7dffdc13b46a5a24d69ab
Bence Balazs <bzs64.bence@gmail.com>
2018-05-10 21:51:44 +0200

    added new tasks solved

---
+++ week-1/day3/12Cuboid/12Cuboid/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _12Cuboid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double side;
+            side = Double.Parse(Console.ReadLine());
+            double surface = side * side;
+            double volume = side * side * side;         
+            Console.WriteLine("Surface area: " + surface);
+            Console.WriteLine("Volume : " + volume);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-1/day3/Secondsinaday/Secondsinaday/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Secondsinaday
+{
+    class Program
+    {
+        private const int V = 60;
+
+        static void Main(string[] args)
+        {
+            int currentHours;
+            int currentMinutes;
+            int currentSeconds;
+             currentHours = Int32.Parse(Console.ReadLine());
+             currentMinutes = Int32.Parse(Console.ReadLine());
+             currentSeconds = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            int remainingHours = 24 - currentHours;
+            int remainingMinutes = 60 - currentMinutes;
+            int remainingSeconds = 60 - currentSeconds;
+
+            Console.WriteLine("Remaining seconds from the day: " +((remainingHours * 60 * 60)+(currentMinutes * 60) + currentSeconds ));
+            Console.ReadLine();
+            // Write a program that prints the remaining seconds (as an integer) from a
+            // day if the current time is represented by the variables
+            //made the original task as well but decided to play with this variation too :)
+        }
+    }
+}
