6e077d85f7ed074a8d303691ecf482ecb6f3715b
Bence Balazs <bzs64.bence@gmail.com>
2018-05-10 14:34:34 +0200

    added new

---
+++ week-1/day3/secondblock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task04");
+            Console.WriteLine();
+            Console.WriteLine("Balázs Bence");
+            Console.WriteLine("23");
+            Console.WriteLine("1.84");
+
+            Console.WriteLine();
+            Console.WriteLine("Task05");
+            Console.WriteLine();
+            Console.WriteLine(13 + 22);
+            Console.WriteLine(13 - 22);
+            Console.WriteLine(22 * 13);
+            Console.WriteLine(22 / 13.00);
+            Console.WriteLine(22 / 13);
+            Console.WriteLine(22 % 13);
+            Console.WriteLine();
+
+            Console.WriteLine("Task06");
+            Console.WriteLine();
+            Console.WriteLine("The number of hours spent with coding: " + 17 * 5 * 6);
+            Console.WriteLine("Percentage in comparison of the whole course " + ((17.0 * 5.0 * 6.0) / (17.0 * 52.0)) * 100.0 + " %");
+
+        }
+    }
+}
--- week-1/day3/thirdblock.cs
+++ week-1/day3/thirdblock.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 
-namespace ConsoleApp2
+namespace ConsoleApp6
 {
     class thirdblock
     {
-        static void Main(string[] args)
+        static void Swap(string[] args)
         {
             Console.WriteLine("Task07");
             Console.WriteLine();
@@ -14,7 +14,19 @@ namespace ConsoleApp2
             Console.WriteLine("Task08");
             Console.WriteLine();
 
-                
+
+            int number1 = 123;
+            int number2 = 526;
+            int temp;
+            Console.WriteLine("Number1 is: " + number1);
+            Console.WriteLine("Number2 is: " + number2);
+            temp = number1;
+            number1 = number2;
+            number2 = temp;
+            Console.Write("\nAfter Swapping : ");
+            Console.Write("\nFirst Number : " + number1);
+            Console.Write("\nSecond Number : " + number2);
+            Console.Read();
         }
     }
 }
