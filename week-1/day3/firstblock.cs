8759ee8ec77c8ec1bbf8ae6132c8faa3a4960d99
Bence Balazs <bzs64.bence@gmail.com>
2018-05-10 12:58:26 +0200

    added third day of prog

---
+++ week-1/day3/firstblock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GFfirstclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task01");
+            Console.WriteLine("Hello Bence!");
+
+            Console.WriteLine("Task02");
+            Console.WriteLine("Humpty Dumpty sat on a wall,");
+            Console.WriteLine("Humpty Dumpty had a great fall.");
+            Console.WriteLine("All the king's horses and all the king's men");
+            Console.WriteLine("Couldn't put Humpty together again.");
+
+            Console.WriteLine("Task03");
+            Console.WriteLine("Hi Zsolti!");
+            Console.WriteLine("Hi Miki!");
+            Console.WriteLine("Hi Máté");
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ week-1/day3/thirdblock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class thirdblock
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task07");
+            Console.WriteLine();
+            int a = 7;
+            Console.WriteLine("My favourite number is: " + a);
+
+            Console.WriteLine("Task08");
+            Console.WriteLine();
+
+                
+        }
+    }
+}
