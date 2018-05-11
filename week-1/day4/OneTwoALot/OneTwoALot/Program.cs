574df1395cf6e90b7305018d41ad64faf5274811
Bence Balazs <bzs64.bence@gmail.com>
2018-05-11 11:27:55 +0200

    added new task

--- week-1/day4/OneTwoALot/OneTwoALot/Program.cs
+++ week-1/day4/OneTwoALot/OneTwoALot/Program.cs
@@ -6,7 +6,31 @@ namespace OneTwoALot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int standard;
+            standard = Int32.Parse(Console.ReadLine());
+            if (standard <= 0)
+            {
+                Console.WriteLine("Not enough");
+            }
+            else if (standard == 1)
+            {
+                Console.WriteLine("One");
+            }
+            else if (standard == 2)
+            {
+                Console.WriteLine("Two");
+            }
+            else
+            {
+                Console.WriteLine("A lot");
+            }
+            Console.ReadLine();
+            // Write a program that reads a number form the standard input,
+            // If the number is zero or smaller it should print: Not enough
+            // If the number is one it should print: One
+            // If the number is two it should print: Two
+            // If the number is more than two it should print: A lot
         }
     }
 }
