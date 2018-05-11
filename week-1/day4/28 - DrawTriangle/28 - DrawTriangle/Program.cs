734b997a0abfc8988740c3fb7085ae6a131166e6
Bence Balazs <bzs64.bence@gmail.com>
2018-05-11 13:58:20 +0200

    core new file

---
+++ week-1/day4/28 - DrawTriangle/28 - DrawTriangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _28___DrawTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Give me a number: ");
+            number = Int32.Parse(Console.ReadLine());
+            string temp = "";
+
+            for (int i = 0; i < number+1; i++)
+            {
+                temp += "*";
+                Console.WriteLine(temp);
+            }
+            Console.ReadLine();
+            // Write a program that reads a number from the standard input, then draws a
+            // triangle like this:
+            //
+            // *
+            // **
+            // ***
+            // ****
+            //
+            // The triangle should have as many lines as the number was
+        }
+    }
+}
