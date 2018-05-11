25f1d090906d53b30445d639414de4a39ed8d2bf
Bence Balazs <bzs64.bence@gmail.com>
2018-05-11 10:12:05 +0200

    added new task

---
+++ week-1/day4/animalsAndLegs/animalsAndLegs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace animalsAndLegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chickens;
+            int pigs;
+
+            chickens = Int32.Parse(Console.ReadLine());
+            pigs = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(chickens * 2 + pigs * 4);
+            Console.ReadLine();
+            // Write a program that asks for two integers
+            // The first represents the number of chickens the farmer has
+            // The second represents the number of pigs owned by the farmer
+            // It should print how many legs all the animals have
+        }
+    }
+}
