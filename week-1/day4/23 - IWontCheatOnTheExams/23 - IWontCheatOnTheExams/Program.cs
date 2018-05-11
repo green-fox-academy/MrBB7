f9a69435fa1359ca5ea993063277324b07bf4bcb
Bence Balazs <bzs64.bence@gmail.com>
2018-05-11 12:44:03 +0200

    added new task

---
+++ week-1/day4/23 - IWontCheatOnTheExams/23 - IWontCheatOnTheExams/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _23___IWontCheatOnTheExams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 99; i++)
+            {
+                Console.WriteLine("I won't cheat on the exam!");
+            }
+            // Create a program that writes this line 100 times:
+            // "I won't cheat on the exam!"
+            Console.ReadLine();
+        }
+    }
+}
