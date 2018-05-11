c36571f9077a71701b39400e6691928f9f015c6a
Bence Balazs <bzs64.bence@gmail.com>
2018-05-11 09:20:10 +0200

    added 14th task

---
+++ week-1/day3/MileToKilometerConv/MileToKilometerConv/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MileToKilometerConv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int km;
+            int mile;
+
+            mile = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(mile * 1.609);
+            Console.ReadLine();
+        }
+    }
+}
