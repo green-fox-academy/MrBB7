c43c5f466528c2dbfd0eda3d43b1cb6f80f96f34
Bence Balazs <bzs64.bence@gmail.com>
2018-05-11 12:50:21 +0200

    added new task

---
+++ week-1/day4/24 - PrintEven/24 - PrintEven/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _24___PrintEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            for (int i = 0; i < 250; i++)
+            {
+                number = number + 2;
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
+            // Create a program that prints all the even numbers between 0 and 500
+        }
+    }
+}
