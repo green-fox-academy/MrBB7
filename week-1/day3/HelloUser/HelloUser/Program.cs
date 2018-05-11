5601e5fc560c86b16bb0ad9b07fdb352d863411b
Bence Balazs <bzs64.bence@gmail.com>
2018-05-11 09:05:03 +0200

    added new task

---
+++ week-1/day3/HelloUser/HelloUser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string user;
+            user = Console.ReadLine();
+            Console.WriteLine("Hello {0} !" , user);
+            Console.ReadLine();
+        }
+    }
+}
