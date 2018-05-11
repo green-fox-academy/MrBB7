4d6c2c514e8b32069b53defe688e1f8d5d402f99
Bence Balazs <bzs64.bence@gmail.com>
2018-05-11 11:04:51 +0200

    added new task

---
+++ week-1/day4/OddEven/OddEven/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OddEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int standard = 5;
+            int newNumber;
+
+            Console.WriteLine(" the standard numebr: " +standard);
+            newNumber = Int32.Parse(Console.ReadLine());
+
+            if (standard == newNumber)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+
+            Console.ReadLine();
+            // Write a program that reads a number from the standard input,
+            // Then prints "Odd" if the number is odd, or "Even" if it is even.
+        }
+    }
+}
