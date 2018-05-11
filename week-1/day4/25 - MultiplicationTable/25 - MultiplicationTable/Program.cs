00e25ecea26185c451dec8c18e75b3b1b4c0cf24
Bence Balazs <bzs64.bence@gmail.com>
2018-05-11 12:58:21 +0200

    added new task solved

---
+++ week-1/day4/25 - MultiplicationTable/25 - MultiplicationTable/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _25___MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+            int number;
+            Console.Write("Please give me a number: ");
+            number = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 10; i++)
+            {
+                result = i * number;
+                Console.Write(i+" * ");
+                Console.Write(number +" = ");
+                Console.WriteLine(result);
+            }
+            Console.ReadLine();
+            // Create a program that asks for a number and prints the multiplication table with that number
+            //
+            // Example:
+            // The number 15 should print:
+            //
+            // 1 * 15 = 15
+            // 2 * 15 = 30
+            // 3 * 15 = 45
+            // 4 * 15 = 60
+            // 5 * 15 = 75
+            // 6 * 15 = 90
+            // 7 * 15 = 105
+            // 8 * 15 = 120
+            // 9 * 15 = 135
+            // 10 * 15 = 150
+        }
+    }
+}
