f54dee86ddbbad89d5043e4c6132c0143d052420
Bence Balazs <bzs64.bence@gmail.com>
2018-05-11 16:10:53 +0200

    added new

---
+++ week-1/day4/29 - DrawPyramid/29 - DrawPyramid/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _29___DrawPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number;
+            Console.Write("Please give me a number: ");
+            number = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j < number -i +1; j++)
+                {                   
+                    Console.Write(" ");
+                }
+                for (int b = 1; b <= i; b++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+            // Write a program that reads a number from the standard input, then draws a
+            // pyramid like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //
+            // The pyramid should have as many lines as the number was
+        }
+    }
+}
