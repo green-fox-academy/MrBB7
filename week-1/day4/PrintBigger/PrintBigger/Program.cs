5c5afd988365f96147ac4c95f4ee8e4d1dbd3394
Bence Balazs <bzs64.bence@gmail.com>
2018-05-11 11:37:31 +0200

    added another task

---
+++ week-1/day4/PrintBigger/PrintBigger/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PrintBigger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberone;
+            int numbertwo;
+            numberone = Int32.Parse(Console.ReadLine());
+            numbertwo = Int32.Parse(Console.ReadLine());
+
+            if (numberone>numbertwo)
+            {
+                Console.WriteLine("the first number is bigger");
+            }
+            else if (numbertwo > numbertwo)
+            {
+                Console.WriteLine("the second number is bigger");
+            }
+            else
+            {
+                Console.WriteLine("the two numbers are even");
+            }
+            Console.ReadLine();
+            // Write a program that asks for two numbers and prints the bigger one
+        }
+    }
+}
