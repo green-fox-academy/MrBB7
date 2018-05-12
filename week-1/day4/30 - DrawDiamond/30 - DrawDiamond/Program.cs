ba63e01ab97983c37ef4ba20e9b2938b29e75f38
Bence Balazs <bzs64.bence@gmail.com>
2018-05-12 21:48:32 +0200

    feat(Workshop):upload solved tasks

---
+++ week-1/day4/30 - DrawDiamond/30 - DrawDiamond/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace _30___DrawDiamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Please give me the number: ");
+            number = Int32.Parse(Console.ReadLine());
+
+            if (number % 2 != 0)
+            {
+                for (int i = 1; i <= (number / 2) + 1; i++)
+                {
+                    for (int j = 0; j < (number - i); j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    for (int k = 1; k < i; k++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+                for (int i = (number) / 2; i >= 1; i--)
+                {
+                    for (int j = 0; j < (number - i); j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    for (int k = 1; k < i; k++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                for (int i = 1; i <= (number / 2); i++)
+                {
+                    for (int j = 0; j < (number - i); j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    for (int k = 1; k < i; k++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+                for (int i = ((number) / 2); i >= 1; i--)
+                {
+                    for (int j = 0; j < (number - i); j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    for (int k = 1; k < i; k++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+  
+                                   // Write a program that reads a number from the standard input, then draws a
+                                   // diamond like this:
+                                   //
+                                   //
+                                   //    *
+                                   //   ***
+                                   //  *****
+                                   // *******
+                                   //  *****
+                                   //   ***
+                                   //    *
+                                   //
+                                   // The diamond should have as many lines as the number was
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-1/day4/31 - DrawSquare/31 - DrawSquare/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _31___DrawSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            string tempstr = "";
+            Console.Write("Please give me the number: ");
+            number = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                if (i == 0 || i == number-1)
+                {
+                    for (int j = 0; j < number; j++)
+                    {
+                        tempstr += "%";
+                    }
+                    Console.WriteLine(tempstr);
+                    tempstr = "";
+                }
+                else
+                {
+                    tempstr += "%";
+                    for (int j = 1; j < number -1; j++)
+                    {
+                        tempstr += " ";
+                    }
+                    tempstr += "%";
+                    Console.WriteLine(tempstr);
+                    tempstr = "";
+                }
+            }
+            Console.ReadLine();
+            // Write a program that reads a number from the standard input, then draws a
+            // square like this:
+            //
+            //
+            // %%%%%
+            // %   %
+            // %   %
+            // %   %
+            // %   %
+            // %%%%%
+            //
+            // The square should have as many lines as the number was
+        }
+    }
+}
