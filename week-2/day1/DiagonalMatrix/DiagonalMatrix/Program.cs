720c6ce50d1a2d22756d010287a49d4b113c37ea
Bence Balazs <bzs64.bence@gmail.com>
2018-05-14 15:48:46 +0200

    feat(Workshop) added new solved task

---
+++ week-2/day1/DiagonalMatrix/DiagonalMatrix/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DiagonalMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[4, 4];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write("1 ");
+                    }
+                    else
+                    {
+                        Console.Write("0 ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+            // - Create (dynamically) a two dimensional array
+            //   with the following matrix. Use a loop!
+            //
+            //   1 0 0 0
+            //   0 1 0 0
+            //   0 0 1 0
+            //   0 0 0 1
+            //
+            // - Print this two dimensional array to the output
+        }
+    }
+}
