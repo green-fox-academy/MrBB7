51867b3805089faaa65396a82cac6540e64983f7
Bence Balazs <bzs64.bence@gmail.com>
2018-05-14 16:14:46 +0200

    feat(Workshop) added new solved task

---
+++ week-2/day1/AppendA/AppendA/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayAnimals = { "kuty", "macsk", "cic" };
+            string append = "a";
+
+            for (int i = 0; i < arrayAnimals.Length; i++)
+            {
+                Console.Write(arrayAnimals[i] + append + " "); 
+            }
+            Console.ReadLine();
+            // - Create an array variable named `animals`
+            //   with the following content: `["kuty", "macsk", "cic"]`
+            // - Add all elements an `"a"` at the end
+        }
+    }
+}
