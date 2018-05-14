b572feeddff2baf9bbaa0beb9f27a32ce0daad7c
Bence Balazs <bzs64.bence@gmail.com>
2018-05-14 15:55:34 +0200

    feat(Workshop) added solved task

---
+++ week-2/day1/DoubleItems/DoubleItems/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DoubleItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynumList = { 3, 4, 5, 6, 7 };
+            arraynumList = Array.ConvertAll(arraynumList, x => x * 2);
+            for (int i = 0; i < arraynumList.Length; i++)
+            {
+                Console.Write(arraynumList[i] +" ");
+            }
+            Console.ReadLine();
+            // - Create an array variable named `numList`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Double all the values in the array
+        }
+    }
+}
