8ca09e0c8ce69181898fc28a3ddba18a2c885352
Bence Balazs <bzs64.bence@gmail.com>
2018-05-14 12:43:19 +0200

    feat(Workshop) added new solved task

---
+++ week-2/day1/Doubling/Doubling/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Doubling
+{
+    class Program
+    {   
+        public static void doubling()
+        {
+            int baseNum = 123;
+            Console.WriteLine(baseNum * 2);
+        }
+        static void Main(string[] args)
+        {
+            doubling();
+            Console.ReadLine();
+
+            // - Create an integer variable named `baseNum` and assign the value `123` to it
+            // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+            // - Print the result of `doubling(baseNum)`
+        }
+    }
+}
