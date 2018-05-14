c6d1d448e72ef832865ea7d2ffa0617f84ca9849
Bence Balazs <bzs64.bence@gmail.com>
2018-05-14 15:04:59 +0200

    feat(Workshop) added new solved task

---
+++ week-2/day1/PrintAll/PrintAll/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PrintAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynumbers = { 4, 5, 6, 7 };
+            for (int i = 0; i < arraynumbers.Length; i++)
+            {
+                Console.WriteLine(arraynumbers[i]);
+            }
+            Console.ReadLine();
+            // - Create an array variable named `numbers`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print all the elements of `numbers`
+        }
+    }
+}
