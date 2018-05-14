a73482ad643081057e3841b12b32515a9fe4e01a
Bence Balazs <bzs64.bence@gmail.com>
2018-05-14 16:10:10 +0200

    feat(Workshop) added new solved task

---
+++ week-2/day1/Colors/Colors/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Colors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] colors = new string[3][];
+            {
+                colors[0] = new string[]{ "lime", "forest green", "olive", "pale green", "spring green" };
+                colors[1] =new string[]{ "orange red", "red", "tomato" };
+                colors[2] = new string[]{ "orchid", "violet", "pink", "hot pink" };
+            };
+            for (int i = 0; i < colors.Length; i++)
+            {
+                for (int j = 0; j < colors[i].Length; j++)
+                {
+                    Console.Write(colors[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+            // - Create a two dimensional array
+            //   which can contain the different shades of specified colors
+            // - In `colors[0]` store the shades of green:
+            //   `"lime", "forest green", "olive", "pale green", "spring green"`
+            // - In `colors[1]` store the shades of red:
+            //   `"orange red", "red", "tomato"`
+            // - In `colors[2]` store the shades of pink:
+            //   `"orchid", "violet", "pink", "hot pink"`
+        }
+    }
+}
