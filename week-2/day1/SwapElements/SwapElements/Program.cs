2af9d36258830c8870d1fe36f44e70cc2496b5af
Bence Balazs <bzs64.bence@gmail.com>
2018-05-14 16:46:25 +0200

    feat(Workshop)added new solved task

---
+++ week-2/day1/SwapElements/SwapElements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SwapElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string temp = "";
+            string[] arrayAbc = { "first", "second", "third" };
+            temp = arrayAbc[0];
+            arrayAbc[0] = arrayAbc[2];
+            arrayAbc[2] = temp;
+
+            for (int i = 0; i < arrayAbc.Length; i++)
+            {
+                Console.Write(arrayAbc[i]+" ");
+            }
+            Console.ReadLine();
+            // - Create an array variable named `abc`
+            //   with the following content: `["first", "second", "third"]`
+            // - Swap the first and the third element of `abc`
+        }
+    }
+}
