0597b00bdc0f2bb969d0417288cf8a8eb134db80
Bence Balazs <bzs64.bence@gmail.com>
2018-05-15 12:00:25 +0200

    add(Workshop) new task

---
+++ week-2/day2/UrlFixer/UrlFixer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace UrlFixer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https//www.reddit.com/r/nevertellmethebots";
+            url = url.Replace("bots", "odds");
+
+            String modifiedurl = url.Insert(5, ":");
+            Console.WriteLine("The modified url: '{0}'", modifiedurl);
+
+
+            // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+            // Also, the URL is missing a crucial component, find out what it is and insert it too!
+            // Try to solve it more than once using different string functions!
+
+            Console.ReadLine();
+        }
+    }
+}
