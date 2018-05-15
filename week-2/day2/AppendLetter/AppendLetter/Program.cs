2ed274cdd6ffd28942a6ec03c0805d54e788d147
Bence Balazs <bzs64.bence@gmail.com>
2018-05-15 15:31:45 +0200

    add(Workshop) add new

---
+++ week-2/day2/AppendLetter/AppendLetter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppendLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+
+            // Create a method called AppendA() that adds "a" to every string in the far list.
+            // The parameter should be a list.
+
+            Console.WriteLine(AppendA(far));
+            Console.ReadLine(); 
+            // Expected output: "kutya", "macska", "kacsa", "róka", "halacska"
+        }
+        public static string AppendA(List<string>endings)
+        { StringBuilder endingBuilder = new StringBuilder();
+
+            for (int i = 0; i < endings.Count; i++)
+            {
+                endingBuilder
+                    .Append(endings[i] +"a ");
+            }
+            return endingBuilder.ToString();
+        }
+    }
+}
