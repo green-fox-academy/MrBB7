22287d11284fe0609023bfb5c11b8583104d54c1
Bence Balazs <bzs64.bence@gmail.com>
2018-05-15 16:23:12 +0200

    add(Workshop) add new

---
+++ week-2/day2/CandyShop/CandyShop/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CandyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<object>();
+            list.Add("Cupcake");
+            list.Add(2);
+            list.Add("Brownie");
+            list.Add(false);
+
+            Console.WriteLine(Sweets(list));
+            Console.ReadLine();
+            // Accidentally we added "2" and "false" to the list.
+            // Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+            // No, don't just remove the lines
+            // Create a method called Sweets() which takes the list as a parameter.
+
+            //  Console.WriteLine(Sweets(list));
+            // Expected output: "Cupcake", "Croissant", "Brownie", "Ice cream"
+        }
+        public static string Sweets(List<object> listOfObjects)
+        {
+            string newdata = "";
+            foreach (var item in listOfObjects)
+            {
+               
+                if (item is bool)
+                {
+                    newdata += "Ice cream, ";
+                }
+                else if (item is int)
+                {
+                    newdata += "Croissant, ";
+                }
+                else
+                {
+                    newdata += item + ", ";
+                }
+            }
+            newdata = newdata.Substring(0, newdata.Length - 2);
+            return newdata;
+        }
+    }
+}
