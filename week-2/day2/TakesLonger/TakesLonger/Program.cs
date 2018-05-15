557ab5af6fcc784e9014e32393eac85a701c5145
Bence Balazs <bzs64.bence@gmail.com>
2018-05-15 12:32:50 +0200

    add(Workshop)new task

---
+++ week-2/day2/TakesLonger/TakesLonger/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace TakesLonger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+            String toFind = "you";
+            int index = quote.IndexOf("you");
+
+            StringBuilder sentenceFixer = new StringBuilder();
+
+            sentenceFixer
+                 .Append("always takes longer than ");
+
+            quote = quote.Substring(0, index) + sentenceFixer + quote.Substring(index, quote.Length - index);
+
+            Console.WriteLine(quote);
+            Console.ReadLine();
+
+            // When saving this quote a disk error has occured. Please fix it.
+            // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
+            // Using pieces of the quote variable (instead of just redefining the string)
+
+        }
+    }
+}
