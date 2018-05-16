25afc580e61354e8c50bed012a2e8a528ad5135e
Bence Balazs <bzs64.bence@gmail.com>
2018-05-16 16:42:11 +0200

    feat(Workshop) start ordering dirs

---
+++ week-2/day3/Logs/Logs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Logs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"log.txt";
+            string[] arrayip = { };
+
+            GiveMeIP(path);
+            Console.WriteLine(File.ReadAllText(path));
+            Console.ReadLine();
+            // Read all data from 'log.txt'.
+            // Each line represents a log message from a web server
+            // Write a function that returns an array with the unique IP adresses.
+            // Write a function that returns the GET / POST request ratio.
+        }
+        public static string[] GiveMeIP(string[,] ipList, string ips)
+        {
+            try
+            {
+                string[,] content = File.ReadAllText(ipList);
+                StreamWriter sw = new StreamWriter(ips);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
