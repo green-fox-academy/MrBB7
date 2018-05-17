6d12094a2d16be355834dc5915e6f638d0e93b7b
Bence Balazs <bzs64.bence@gmail.com>
2018-05-17 12:11:02 +0200

    add(Workshop) new task

---
+++ week-2/day4/02-ColoredBox/02-ColoredBox/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _02_ColoredBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // Draw a box that has different colored lines on each edge.
+            foxDraw.StrokeColor(Colors.Pink);
+            foxDraw.DrawLine(50, 100, 100, 100, 10);
+
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(100, 100, 100, 200, 10);
+
+            foxDraw.StrokeColor(Colors.Blue);
+            foxDraw.DrawLine(100, 200, 50, 200, 10);
+
+            foxDraw.StrokeColor(Colors.Yellow);
+            foxDraw.DrawLine(50, 200, 50, 100, 10);
+
+        }
+    }
+}
