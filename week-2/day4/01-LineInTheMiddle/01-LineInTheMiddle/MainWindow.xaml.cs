f1289bb6becdabf1a44799f8471d73eb175558e4
Bence Balazs <bzs64.bence@gmail.com>
2018-05-17 11:47:24 +0200

    add(Workshop) add new task

---
+++ week-2/day4/01-LineInTheMiddle/01-LineInTheMiddle/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _01_LineInTheMiddle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine( canvas.Width/2, 0, canvas.Height /2 , canvas.Height);
+
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(canvas.Width,canvas.Height/2, 0, canvas.Height/2);
+        }
+    }
+}
