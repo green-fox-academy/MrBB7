6377fc1ec758dd50503ec0e47f2303c9556a0560
Bence Balazs <bzs64.bence@gmail.com>
2018-05-17 12:19:48 +0200

    add(Workshop) new task

---
+++ week-2/day4/03-Diagonals/03-Diagonals/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _03_Diagonals
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(0, 0, canvas.Width, canvas.Height);
+
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(0, canvas.Height, canvas.Width,0);
+            //Draw the canvas' diagonals.
+            // If it starts from the upper-left corner it should be green, otherwise it should be red.
+        }
+    }
+}
