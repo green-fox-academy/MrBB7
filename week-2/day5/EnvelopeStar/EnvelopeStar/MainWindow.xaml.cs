ae0026e2cd85bd6a134ccbe8eb9e93572a8baef7
Bence Balazs <bzs64.bence@gmail.com>
2018-05-18 11:20:46 +0200

    add(EnvelopeStar) working function

---
+++ week-2/day5/EnvelopeStar/EnvelopeStar/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EnvelopeStar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            
+
+            for (int i = 15; i < 300; i+=15)
+            {
+                 foxDraw.StrokeColor(Colors.Green);      //right bottom
+                 foxDraw.DrawLine(canvas.Width/2, (canvas.Height/2)+i, canvas.Width-i, canvas.Height /2);
+
+                 foxDraw.StrokeColor(Colors.Green);    //left bottom
+                 foxDraw.DrawLine(0 + i, canvas.Height / 2, canvas.Width / 2, canvas.Height / 2 + i);
+
+                 foxDraw.StrokeColor(Colors.Green);   //top right
+                 foxDraw.DrawLine(canvas.Width -i, canvas.Height/2, canvas.Width/2, canvas.Height / 2-i);
+
+                 foxDraw.StrokeColor(Colors.Green);  //top left
+                 foxDraw.DrawLine(0+i, canvas.Height/2, canvas.Width/2, canvas.Height / 2-i);
+
+
+            }
+        }
+    }
+}
