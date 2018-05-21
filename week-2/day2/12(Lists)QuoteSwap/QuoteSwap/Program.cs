698157cc0afbfb962feeea4a9a1a6f767abaa31a
Bence Balazs <bzs64.bence@gmail.com>
2018-05-21 13:37:21 +0200

    add(QuoteSwap) working function

--- week-2/day2/12(Lists)QuoteSwap/QuoteSwap/Program.cs
+++ week-2/day2/12(Lists)QuoteSwap/QuoteSwap/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace QuoteSwap
 {
@@ -6,7 +8,33 @@ namespace QuoteSwap
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var list = new List<string> { "What", "I", "do", "create,", "I", "cannot", "not", "understand." };
+
+            // Accidentally I messed up this quote from Richard Feynman.
+            // Two words are out of place
+            // Your task is to fix it by swapping the right words with code
+            // Create a method called QuoteSwap().
+
+            // Also, print the sentence to the output with spaces in between.
+            // Expected output: "What I cannot create I do not understand."     
+            Console.WriteLine(QuoteSwap(list));
+            Console.ReadLine();
+
+        }
+        public static string QuoteSwap(List<string>words)
+        {
+            StringBuilder temp = new StringBuilder();
+            int indexOne = words.IndexOf("do");
+            int indexTwo = words.IndexOf("cannot");
+
+            words[indexOne] = "cannot";
+            words[indexTwo] = "do";
+
+            foreach (string item in words)
+            {
+                temp.Append(item + " ");
+            }
+            return temp.ToString();
         }
     }
 }
