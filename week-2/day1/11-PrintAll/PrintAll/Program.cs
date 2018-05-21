664788c9106a8889935f3a4da15dd915affc69f3
Bence Balazs <bzs64.bence@gmail.com>
2018-05-21 14:42:03 +0200

    feat(PrintAll) refactor code based on comments

--- week-2/day1/11-PrintAll/PrintAll/Program.cs
+++ week-2/day1/11-PrintAll/PrintAll/Program.cs
@@ -6,10 +6,10 @@ namespace PrintAll
     {
         static void Main(string[] args)
         {
-            int[] arraynumbers = { 4, 5, 6, 7 };
-            for (int i = 0; i < arraynumbers.Length; i++)
+            int[] numbers = { 4, 5, 6, 7 };
+            for (int i = 0; i < numbers.Length; i++)
             {
-                Console.WriteLine(arraynumbers[i]);
+                Console.WriteLine(numbers[i]);
             }
             Console.ReadLine();
             // - Create an array variable named `numbers`
