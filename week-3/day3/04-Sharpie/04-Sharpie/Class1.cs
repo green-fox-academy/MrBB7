b8a238079eacc4007afec88d9f7327a479d072ec
Bence Balazs <bzs64.bence@gmail.com>
2018-05-23 16:04:08 +0200

    core(Sharpie) working function

---
+++ week-3/day3/04-Sharpie/04-Sharpie/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharpie
+{
+    class Sharpie
+    {
+        public string Color;
+        public float Width;
+        public float inkAmount;
+
+        public Sharpie(string color, float width)
+        {
+            this.Color = color;
+            this.Width = width;
+            inkAmount = 100;
+        }
+        public float Use()
+        {
+           
+            inkAmount-=Width;
+
+            return inkAmount;
+        }
+    }
+}
---
+++ week-3/day3/04-Sharpie/04-Sharpie/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sharpie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sharpie firstSharpie = new Sharpie("orange" , 13);
+            Sharpie secondSharpie = new Sharpie("blue", 41);
+
+            firstSharpie.Use();
+            secondSharpie.Use();
+
+            Console.WriteLine( "the level of ink is: " + firstSharpie.inkAmount);
+            Console.WriteLine("the level of ink is: " + secondSharpie.inkAmount);
+            Console.ReadLine();
+        }
+    }
+}
