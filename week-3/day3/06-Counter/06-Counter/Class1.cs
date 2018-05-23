fff6c9368fc64f1dff7c7ab601f0dbe02e9723d2
Bence Balazs <bzs64.bence@gmail.com>
2018-05-23 20:39:05 +0200

    feat(Counter) working func

---
+++ week-3/day3/06-Counter/06-Counter/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Counter
+{
+    class Counter
+    {
+        public int sum;
+
+        public Counter()
+        {
+            sum = 0;
+        }
+        public int Add(int number)
+        {
+            sum += number;
+            return sum;
+        }
+        public void Add()
+        {
+            sum++;
+        }
+        public string Get()
+        {
+            string currentSum = sum.ToString();
+            return currentSum;
+        }
+        public void Reset()
+        {
+            sum = 0;
+        }
+    }
+}
---
+++ week-3/day3/06-Counter/06-Counter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter sum = new Counter();
+
+            sum.Reset();
+            Console.WriteLine(sum.Get());
+            Console.ReadLine();
+        }
+    }
+}
