0c15b45d8d115b70bd0e088bbf84f1a3245ffb39
Bence Balazs <bzs64.bence@gmail.com>
2018-05-24 12:42:50 +0200

    chore(BunniesAgain) working func

---
+++ week-3/day4/06-BunniesDeNuevo/06-BunniesDeNuevo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _06_BunniesDeNuevo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+            // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+            // have 3 ears, because they each have a raised foot. Recursively return the
+            // number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+            Console.WriteLine(BunniesInTheEar(Int32.Parse(Console.ReadLine())));
+            Console.ReadLine();
+
+        }
+
+        public static int BunniesInTheEar(int number)
+        {
+            if (number == 0)
+            {
+                return 0;
+            }
+            else if (number % 2 == 0)
+            {
+                return 3 + (BunniesInTheEar(number - 1));
+            }
+            else
+            {
+                return 2 + (BunniesInTheEar(number - 1));
+            }
+        }
+    }
+}
