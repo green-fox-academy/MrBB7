1a916a84f25c698818a3667face02cd84b482916
Bence Balazs <bzs64.bence@gmail.com>
2018-05-24 10:48:45 +0200

    chore(Counter) working function

---
+++ week-3/day4/01-Counter/01-Counter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Counter(Int32.Parse(Console.ReadLine())));
+            Console.ReadLine();
+        }
+
+        public static int Counter(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine(n);
+                return Counter(n - 1);
+            }
+        }
+    }
+}
