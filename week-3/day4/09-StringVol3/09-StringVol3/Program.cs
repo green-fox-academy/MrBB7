4e921695033f35b03f103d927e3f5cefb8921af1
Bence Balazs <bzs64.bence@gmail.com>
2018-05-24 16:10:28 +0200

    chore(StringVol3) working func

---
+++ week-3/day4/09-StringVol3/09-StringVol3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _09_StringVol3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively a new string where all the
+            // adjacent chars are now separated by a "*".
+
+            Console.WriteLine(CharAdd(Console.ReadLine()));
+            Console.ReadLine();
+
+        }
+
+        private static string CharAdd(string inputText)
+        {
+            char space = '*';
+            if (inputText == "")
+            {
+                return null;
+            }
+
+                if (inputText.Length == 1)
+                {
+                space = '\0';   
+                }
+
+                return inputText[0].ToString() + space.ToString() + CharAdd(inputText.Remove(0, 1));
+            }
+        }
+    }
+
