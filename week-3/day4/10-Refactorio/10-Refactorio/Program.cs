eb89bc46f9d615af1a34fba9cc39cdd7bb3d1fbe
Bence Balazs <bzs64.bence@gmail.com>
2018-05-24 16:21:42 +0200

    chore(Factorio) working func

---
+++ week-3/day4/10-Refactorio/10-Refactorio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _10_Refactorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(Int32.Parse(Console.ReadLine())));
+            Console.ReadLine();
+        }
+
+        private static int Factorial(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Factorial(number - 1);
+            }
+        }
+    }
+}
