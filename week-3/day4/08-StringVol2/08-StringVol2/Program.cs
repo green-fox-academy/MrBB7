b8307149a3726a12e08c266948e230e8054ce5d0
Bence Balazs <bzs64.bence@gmail.com>
2018-05-24 14:10:12 +0200

    chore(StringVol2) working func

---
+++ week-3/day4/08-StringVol2/08-StringVol2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _08_StringVol2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively a new string where all the 'x' chars have been removed.
+
+            Console.WriteLine(RemoveX(Console.ReadLine()));
+            Console.ReadLine();
+
+
+        }
+
+        private static string RemoveX(string inputText)
+        {
+            if (inputText == "")
+            {
+                return null;
+            }
+
+            else
+            {
+                if (inputText[0] == 'x')
+                {
+                    return RemoveX(inputText.Remove(0, 1));
+                }
+                else
+                {
+                    return inputText[0] + RemoveX(inputText.Remove(0, 1));
+                }
+            }
+        }
+    }
+}
