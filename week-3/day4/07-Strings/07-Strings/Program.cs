8f073a80063338e16879f26e93c663b7504364d5
Bence Balazs <bzs64.bence@gmail.com>
2018-05-24 13:19:34 +0200

    chore(Strings) working func

---
+++ week-3/day4/07-Strings/07-Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively (no loops) a new string where all the
+            // lowercase 'x' chars have been changed to 'y' chars.
+
+            Console.WriteLine(Yalizer(Console.ReadLine()));
+            Console.ReadLine();
+        }
+
+        public static string Yalizer(string inputText)
+        {
+            string tempWord = inputText;
+            if (tempWord.Contains("x"))
+            {
+                return tempWord = Yalizer(tempWord.Replace("x","y"));
+            }
+            else
+            {
+                return tempWord;
+            }
+        }
+    }
+}
