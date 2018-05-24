06566fcd6d79c7ad97e939add24c52fede7bc4de
Bence Balazs <bzs64.bence@gmail.com>
2018-05-24 11:02:24 +0200

    chore(NumberAdder) working function

---
+++ week-3/day4/02-NumberAdder/02-NumberAdder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02_NumberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Summarizer(Int32.Parse(Console.ReadLine())));
+            Console.ReadLine();
+        }
+
+        private static int Summarizer(int number)
+        {
+            int sum = 0;
+            if (number == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine(number);
+                return number + Summarizer(number - 1);
+            }
+        }
+    }
+}
