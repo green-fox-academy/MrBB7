4a67a376c6fc1592c4810bf20351c35195719218
Bence Balazs <bzs64.bence@gmail.com>
2018-05-24 16:14:22 +0200

    chore(Fibonacci) MatReview task

---
+++ week-3/day4/MatRev(FibonacciRecursion)/MatRev(Recursion)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MatRev_Recursion_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //0,1,1,2,3,5,8,13,21
+
+            Console.WriteLine(GetFibonacci(30));
+            Console.ReadLine();
+        }
+
+        public static int GetFibonacci(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+            }
+        }
+    }
+}
