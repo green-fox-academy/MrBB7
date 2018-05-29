674edef81f99869faeead6593ec5dcfe6a370d7e
Bence Balazs <bzs64.bence@gmail.com>
2018-05-29 20:21:53 +0200

    feat(PostIt) refactor

---
+++ week-3/day4/11-FirstFractal/11-FirstFractal/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _11_FirstFractal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+                int xfirst = 0;
+                int yfirst = 0;
+            int divider = 3; 
+
+            if (xfirst == 3 && yfirst == 3)
+            {
+                break;
+            }
+            else
+            {
+                return 
+            }
+
+                foxDraw.FillColor(Colors.Yellow);
+                foxDraw.DrawRectangle(xfirst+=divider, yfirst+=divider, canvas.Width, canvas.Height);
+            }
+            
+
+        }
+    }
+}
---
+++ week-3/day5/TrianglesFractal/TrianglesFractal/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFExample;
+
+namespace TrianglesFractal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FoxDraw foxDraw;
+        public MainWindow()
+        {
+            InitializeComponent();
+            foxDraw = new FoxDraw(canvas);
+
+            DrawTriangle(15, new Point(400,0), new Point(0,800), new Point(800,0));
+
+        }
+
+        public void DrawTriangle(int level, new Point topPoint, new Point leftPoint,new Point rightPoint)
+        {
+            if (level == 0)
+            {
+                List<Point> points = { topPoint, leftPoint, rightPoint };
+                foxDraw.FillColor(Colors.Purple);
+            }
+            else
+            {
+                List<Point> leftMid = new List<Point>();
+                leftMid.Add(new Point((topPoint + leftPoint) / 2, (topPoint + leftPoint) / 2));
+                List<Point> rightMid = new List<Point>();
+                 rightMid.Add(new Point(topPoint + rightPoint) / 2, (topPoint + rightPoint) / 2));
+                List<Point> bottomMid = new List<Point>();
+                bottomMid.Add(new Point(leftPoint + rightPoint) / 2, (leftPoint + rightPoint) / 2));
+
+                List<Point> endPoints = new List<Point>();
+                endPoints.Add(new Point((topPoint + leftPoint) / 2, (topPoint + leftPoint) / 2));
+                endPoints.Add(new Point(topPoint + rightPoint) / 2, (topPoint + rightPoint) / 2));
+                endPoints.Add(new Point(leftPoint + rightPoint) / 2, (leftPoint + rightPoint) / 2));
+
+                foxDraw.DrawPolygon(endPoints);
+
+
+                DrawTriangle(level - 1, topPoint, leftMid, rightMid);
+                DrawTriangle(level - 1, leftMid, leftPoint, bottomMid);
+                DrawTriangle(level - 1, rightMid, bottomMid, rightPoint);
+            }         
+        }
+    }   
+}
