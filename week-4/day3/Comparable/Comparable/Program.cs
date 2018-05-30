fe84fdabdba68e09e85763d9f87a1ea3bc344e6c
Bence Balazs <bzs64.bence@gmail.com>
2018-05-30 14:03:46 +0200

    chore(Comparable) basics

---
+++ week-4/day3/Comparable/Comparable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Comparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
