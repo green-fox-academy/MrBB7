ded51dace5d5b2a6430dc52437a9d88d5bb30169
Bence Balazs <bzs64.bence@gmail.com>
2018-05-30 12:48:09 +0200

    chore(Cloneable) working func

---
+++ week-4/day3/Cloneable/Cloneable/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GreenFoxOrganization;
+
+namespace Cloneable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student john = new Student("John", 20, "male", "BME");
+            Object clone = john.Clone();
+            Console.WriteLine(clone);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ week-4/day3/Cloneable/Cloneable/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenFoxOrganization
+{
+    class Student : ICloneable
+    {
+        private string name;
+        private int age;
+        private string gender;
+        private string previousOrganization;
+
+        public Student(string name, int age, string gender, string previousOrganization)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.previousOrganization = previousOrganization;
+        }
+
+        public object Clone()
+        {
+            Student clonedStudent = new Student(name, age, gender, previousOrganization);
+            return clonedStudent;
+        }
+
+        public override string ToString()
+        {
+            return name + " " + age + " " + gender + " " + previousOrganization;
+        }
+    }
+}
+
