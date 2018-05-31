b82cf9409bba0b31136c9583cd9dc340e6a23d18
Bence Balazs <bzs64.bence@gmail.com>
2018-05-31 16:16:06 +0200

    chore(Sum) working test

---
+++ week-4/day4/Sum/ClassLibrary1/Test.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sum;
+
+namespace Test
+{
+    [TestFixture]
+    public class Test
+    {
+        [TestCase]
+        public void ShouldBeTheSame()
+        {
+            TestSum justAList = new TestSum();
+            List<int> testNumbers = new List<int>() { 10, 10, 10, 10 };
+            Assert.AreEqual(justAList.Summerizer(testNumbers), 40);
+        }
+    }
+}
+
---
+++ week-4/day4/Sum/Sum/Sum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Sum
+{
+    public class TestSum
+    {
+        public int Summerizer(List<int> numbers)
+        {
+            int summed = numbers.Sum();
+            return summed;
+        }
+    }
+}
+
