086fe45db4410039a58f496a44ec4485c1cb4f37
Bence Balazs <bzs64.bence@gmail.com>
2018-05-31 13:20:45 +0200

    chore(Apples) working test

---
+++ week-4/day4/Apples/Apples/Apple.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apples
+{
+        public class Apple
+        {
+
+        public string GetApple()
+        {
+            return "apple";
+        }
+    }
+}
---
+++ week-4/day4/Apples/TestEnvironment/Class1.cs
@@ -0,0 +1,37 @@
+﻿using Apples;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEnvironment
+{
+    [TestFixture]
+    public class Class1
+    {
+        
+         private Apple justAnApple;
+
+         [SetUp]
+         public void SetUp()
+         {
+             justAnApple = new Apple();
+         }
+
+         [TearDown]
+         public void TearDown()
+         {
+             justAnApple = null;
+         }
+
+         [TestCase("apple")]
+         public void Tester(string apple)
+         {
+             Assert.AreEqual(apple, justAnApple.GetApple());
+         }
+    }
+}
+
+
