6c6bb01dc0fcc6696bca46a77a6f54c0b40dddd7
Bence Balazs <bzs64.bence@gmail.com>
2018-06-18 17:17:20 +0200

    chore(OddAverage) working func

---
+++ week-1(Orientation)/OddAverage/OddAverage/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OddAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            IEnumerable<int> oddNumbers =
+                from number in numbers
+                where number % 2 == 0
+                select number;
+
+            foreach (int item in oddNumbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            double average = oddNumbers.Average();
+
+            Console.WriteLine("The average grade is {0}.", average);
+
+            IEnumerable<int> MethodOddNumbers = numbers.Where(number => number % 2 == 0).OrderBy(n => n);
+
+            foreach (int item in MethodOddNumbers)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
