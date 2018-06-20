ed1d01f1701d4d79aa423e0466773935dfac886a
Bence Balazs <bzs64.bence@gmail.com>
2018-06-20 19:41:34 +0200

    chore(FreqFinder) working function

---
+++ week-1(Orientation)/day1/FreqFinder/FreqFinder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FreqFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the frequency of numbers in the following array:
+
+            int[] numbers = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var freqFinder = from number in numbers
+                             orderby number
+                             group number by number into grupo
+                             select new { Value = grupo.Count(), Key = grupo.Key };
+
+            foreach (var item in freqFinder)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+
+            var methodSnytFinder = numbers.GroupBy(number => number);
+
+            foreach (var item in methodSnytFinder)
+            {
+                Console.WriteLine(item.Key + " " + item.Count());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-1(Orientation)/day1/GetEvenNumbers/GetEvenNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GetEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            IEnumerable<int> oddNumbers =
+                from number in numbers
+                where number % 2 == 0
+                select number;
+
+            foreach (int item in oddNumbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            IEnumerable<int> MethodOddNumbers = numbers.Where(number => number % 2 == 0).OrderBy(n => n);
+
+            foreach (int item in MethodOddNumbers)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
