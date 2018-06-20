047fa27ce68857b7d7a694fa135f947abfe36834
Bence Balazs <bzs64.bence@gmail.com>
2018-06-20 20:59:41 +0200

    chore(CharFreq) basics

---
+++ week-1(Orientation)/day1/CharFreq/CharFreq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CharFreq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
