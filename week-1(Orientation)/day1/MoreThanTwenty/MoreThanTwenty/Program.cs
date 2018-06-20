eeb1706506e7242b354119cc81535a5d33489aff
Bence Balazs <bzs64.bence@gmail.com>
2018-06-20 18:51:39 +0200

    chore(MoreThenTwenty) working function

---
+++ week-1(Orientation)/day1/MoreThanTwenty/MoreThanTwenty/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MoreThanTwenty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Write a LINQ Expression to find which number squared value is more then 20 from the following array:
+
+            int[] numbers = new[] { 3, 9, 2, 8, 6, 5 };
+
+            IEnumerable<int> moreThenTwenty = from number in numbers
+                                              where number * number > 20
+                                              select number;
+
+            foreach (var item in moreThenTwenty)
+            {
+                Console.WriteLine(item);
+            }
+
+            IEnumerable<int> MethodSyntMoreThenTwenty = numbers.Select(number => number * number).Where(number => number > 20);
+
+            foreach (var item in MethodSyntMoreThenTwenty)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
