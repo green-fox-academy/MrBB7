4dfef7cb5e6793b49e1011567cbd2d13324c0924
Bence Balazs <bzs64.bence@gmail.com>
2018-06-20 21:02:19 +0200

    feat(CharFreq) working function

--- week-1(Orientation)/day1/CharFreq/CharFreq/Program.cs
+++ week-1(Orientation)/day1/CharFreq/CharFreq/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace CharFreq
 {
@@ -6,7 +8,29 @@ namespace CharFreq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Write a LINQ Expression to find the frequency of characters in a given string!
+
+            string characterChain= "Somethingrandomquote";
+
+            var findCharFreq = from character in characterChain.ToLower()
+                               group character by character into grupo
+                               select new { Value = grupo.Count(), Key = grupo.Key };
+
+            foreach (var item in findCharFreq)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+
+            var methodSnytCharFinder = characterChain.GroupBy(number => number);
+
+            foreach (var item in methodSnytCharFinder)
+            {
+                Console.Write(item.Key + " " + item.Count());
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
