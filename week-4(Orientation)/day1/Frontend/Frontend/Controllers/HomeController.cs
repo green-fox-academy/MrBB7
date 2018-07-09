a7fed14637d73f564114df4f8c1cb0a0bb1e7456
Bence Balazs <bzs64.bence@gmail.com>
2018-07-09 12:16:31 +0200

    chore(Frontend) add first task

---
+++ week-4(Orientation)/day1/Frontend/Frontend/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return File("index.html", "text/html");
+        }
+    }
+}
