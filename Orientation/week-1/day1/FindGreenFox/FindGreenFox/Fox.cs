bf30192408a25ad1de44fe134a337b2b836e9a84
Bence Balazs <bzs64.bence@gmail.com>
2018-07-23 12:09:27 +0200

    feat(FindGreenFox) add solved task

---
+++ Orientation/week-1/day1/FindGreenFox/FindGreenFox/Fox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindGreenFox
+{
+    class Fox
+    {
+        public Fox(string name, string type, string color)
+        {
+            Name = name;
+            Type = type;
+            Color = color;
+        }
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ Orientation/week-1/day1/FindGreenFox/FindGreenFox/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindGreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Fox> foxes = new List<Fox>
+            {
+                new Fox("Vuk", "pallida", "green"),
+                new Fox("Karak", "fulvipes", "green"),
+                new Fox("yMaxy", "pallida", "grey"),
+                new Fox("Tom", "cat", "greyish"),
+                new Fox("Jerry", "mouse", "brown"),
+            };
+
+            var lambda = foxes.Where(x =>  x.Color == "green")
+                                          .Select(x => x.Name);            
+
+            var query = from fox in foxes
+                        where fox.Type == "pallida" && fox.Color == "green"
+                        select fox.Name;
+
+            Console.Write(string.Join(", ", lambda) + ',');
+
+            Console.Write(string.Join(", ", query));
+
+            Console.ReadLine();
+        }
+    }
+}
