c08750eec98a4e18db238b0e076120548d190a00
Bence Balazs <bzs64.bence@gmail.com>
2018-07-23 11:22:43 +0200

    feat(ToStringConverter): add solved task

---
+++ Orientation/week-1/day1/ToStringConverter/ToStringConverter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToStringConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] inputChars = new char[] { 'H', 'e', 'l', 'l', 'o' };
+
+            var lambda = string.Join("", inputChars.Select(x => x.ToString()));
+
+            var query = from character in inputChars
+                        select character.ToString();
+
+            Console.WriteLine(lambda);
+
+            Console.WriteLine(string.Join("", query));
+
+            Console.ReadLine();
+
+        }
+    }
+}
