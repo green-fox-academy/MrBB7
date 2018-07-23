fab42b641d48ab9ecba5c4d8ba18108bd8b83f23
Bence Balazs <bzs64.bence@gmail.com>
2018-07-23 10:50:44 +0200

    chore(FindUpperCase): add solved task

---
+++ Orientation/week-1/day1/FindUpperCase/FindUpperCase/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindUpperCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var query = (from character in input
+                         where character.ToString().Equals(character.ToString().ToUpper())
+                         select character);
+
+            Console.WriteLine(string.Join(",",query));
+
+            Console.ReadLine();
+        }
+    }
+}
