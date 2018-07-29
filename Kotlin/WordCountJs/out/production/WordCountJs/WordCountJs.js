05d2019edda134559d3c386b9cd62ea9f58d41d2
Bence Balazs <bzs64.bence@gmail.com>
2018-07-29 16:49:43 +0200

    feat(WordCounterJs) word counter web application

--- Kotlin/WordCountJs/out/production/WordCountJs/WordCountJs.js
+++ Kotlin/WordCountJs/out/production/WordCountJs/WordCountJs.js
@@ -6,11 +6,71 @@ var WordCountJs = function (_, Kotlin) {
   var println = Kotlin.kotlin.io.println_s8jyv4$;
   var ensureNotNull = Kotlin.ensureNotNull;
   var throwCCE = Kotlin.throwCCE;
+  var replace = Kotlin.kotlin.text.replace_680rmw$;
+  var split = Kotlin.kotlin.text.split_ip8yn$;
+  var equals = Kotlin.equals;
+  var capitalize = Kotlin.kotlin.text.capitalize_pdl1vz$;
+  var toList = Kotlin.kotlin.collections.toList_abgq59$;
+  var sortedWith = Kotlin.kotlin.collections.sortedWith_eknfly$;
   var Unit = Kotlin.kotlin.Unit;
+  function main$lambda$lambda(it) {
+    return it.second;
+  }
+  var LinkedHashMap_init = Kotlin.kotlin.collections.LinkedHashMap_init_q3lmfv$;
+  var wrapFunction = Kotlin.wrapFunction;
+  var compareByDescending$lambda = wrapFunction(function () {
+    var compareValues = Kotlin.kotlin.comparisons.compareValues_s00gnj$;
+    return function (closure$selector) {
+      return function (a, b) {
+        var selector = closure$selector;
+        return compareValues(selector(b), selector(a));
+      };
+    };
+  });
+  var Kind_CLASS = Kotlin.Kind.CLASS;
+  var Comparator = Kotlin.kotlin.Comparator;
+  function Comparator$ObjectLiteral(closure$comparison) {
+    this.closure$comparison = closure$comparison;
+  }
+  Comparator$ObjectLiteral.prototype.compare = function (a, b) {
+    return this.closure$comparison(a, b);
+  };
+  Comparator$ObjectLiteral.$metadata$ = {kind: Kind_CLASS, interfaces: [Comparator]};
   function main$lambda(it) {
-    var tmp$;
-    var theText = (Kotlin.isType(tmp$ = ensureNotNull(document.getElementById('wordText')), HTMLInputElement) ? tmp$ : throwCCE()).value;
-    println(theText);
+    var tmp$, tmp$_0, tmp$_1, tmp$_2;
+    var allTheWords = (Kotlin.isType(tmp$ = ensureNotNull(document.getElementById('wordText')), HTMLInputElement) ? tmp$ : throwCCE()).value;
+    var words = split(replace(replace(replace(allTheWords, '\n', ' '), ',', ''), '.', ''), [' ']);
+    var wordMap = LinkedHashMap_init();
+    tmp$_0 = words.iterator();
+    while (tmp$_0.hasNext()) {
+      var word = tmp$_0.next();
+      if (!equals(word, '')) {
+        if (wordMap.get_11rb$(capitalize(word)) == null) {
+          var key = capitalize(word);
+          wordMap.put_xwzc9p$(key, 1);
+        }
+         else {
+          var wordCount = ensureNotNull(wordMap.get_11rb$(capitalize(word)));
+          var key_0 = capitalize(word);
+          var value = wordCount + 1 | 0;
+          wordMap.put_xwzc9p$(key_0, value);
+        }
+      }
+    }
+    var wordList = toList(wordMap);
+    var sortedList = sortedWith(wordList, new Comparator$ObjectLiteral(compareByDescending$lambda(main$lambda$lambda)));
+    tmp$_1 = sortedList.iterator();
+    while (tmp$_1.hasNext()) {
+      var word_0 = tmp$_1.next();
+      println(word_0);
+    }
+    var listItems = '';
+    tmp$_2 = sortedList.iterator();
+    while (tmp$_2.hasNext()) {
+      var word_1 = tmp$_2.next();
+      listItems += '<li>' + word_1.first + ' - ' + word_1.second + '<\/li>';
+    }
+    ensureNotNull(document.getElementById('List')).innerHTML = listItems;
     return Unit;
   }
   function main(args) {
--- Kotlin/WordCountJs/src/WordCountJS.kt
+++ Kotlin/WordCountJs/src/WordCountJS.kt
@@ -6,7 +6,36 @@ fun main(args: Array<String>) {
 
     val button = document.getElementById("button")!!
     button.addEventListener("click", {
-       val theText = (document.getElementById("wordText")!! as HTMLInputElement).value
-        println(theText)
+       val allTheWords = (document.getElementById("wordText")!! as HTMLInputElement).value
+
+        val words = allTheWords.replace("\n", " ").replace(",", "").replace(".", "").split(" ")
+
+        val wordMap = mutableMapOf<String, Int>()
+        for (word in words) {
+            if (word != ""){
+
+                if (wordMap[word.capitalize()] == null) {
+                    wordMap[word.capitalize()] = 1
+                } else {
+                    val wordCount = wordMap[word.capitalize()]!!
+                    wordMap[word.capitalize()] = wordCount + 1
+                }
+            }
+        }
+
+        val wordList = wordMap.toList()
+
+        val sortedList = wordList.sortedWith(compareByDescending({it.second}))
+
+        for (word in sortedList) {
+            println(word)
+        }
+
+        var listItems = ""
+        for (word in sortedList) {
+            listItems += "<li>${word.first} - ${word.second}</li>"
+        }
+
+        document.getElementById("List")!!.innerHTML = listItems
     })
 }
